0ec9f4f00392b4100aa03ed4e2bb3cb560e832ba
hapo-vupq <vupq@haposoft.com>
2022-07-15 14:05:26 +0700

    Fix null profile

--- android/src/main/java/com/facebook/reactnative/androidsdk/FBProfileModule.java
+++ android/src/main/java/com/facebook/reactnative/androidsdk/FBProfileModule.java
@@ -16,9 +16,10 @@ import androidx.annotation.NonNull;
 @ReactModule(name = FBProfileModule.NAME)
 public class FBProfileModule extends ReactContextBaseJavaModule {
     public static final String NAME = "FBProfile";
+    private ProfileTracker mProfileTracker;
 
     public FBProfileModule(ReactApplicationContext reactContext) {
-      super(reactContext);
+        super(reactContext);
     }
 
     @NonNull
@@ -33,9 +34,16 @@ public class FBProfileModule extends ReactContextBaseJavaModule {
     */
     @ReactMethod
     public void getCurrentProfile(Callback callback) {
-        //Return the profile object as a ReactMap.
-        callback.invoke(Profile.getCurrentProfile() == null
-                ? null
-                : Utility.profileToReactMap(Profile.getCurrentProfile()));
+      if (Profile.getCurrentProfile() == null) {
+        mProfileTracker = new ProfileTracker() {
+          @Override
+          protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+            callback.invoke(Utility.profileToReactMap(currentProfile));
+            mProfileTracker.stopTracking();
+          }
+        };
+      } else {
+        callback.invoke(Utility.profileToReactMap(Profile.getCurrentProfile()));
+      }
   }
 }
